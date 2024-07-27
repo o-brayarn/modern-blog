3b8af77edaa2ff7ead9f66d15e14eb5be6c47f9a
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-27 09:45:12 +0300

    create blog pages

---
+++ src/pages/Article.jsx
@@ -0,0 +1,9 @@
+const Article = () => {
+  return (
+    <div>
+      <h1>This is the Article page</h1>
+    </div>
+  );
+};
+
+export default Article;
---
+++ src/pages/ArticlesList.jsx
@@ -0,0 +1,9 @@
+const ArticlesList = () => {
+  return (
+    <div>
+      <h1>This is the articles list page</h1>
+    </div>
+  );
+};
+
+export default ArticlesList;
