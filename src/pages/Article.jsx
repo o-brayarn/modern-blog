3be43c771acc4de26d2a01c754302b352692e41f
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-27 10:30:48 +0300

    url parameters using react-router-dom useParams

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,8 +1,16 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const Article = () => {
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
   return (
-    <div>
-      <h1>This is the Article page</h1>
-    </div>
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph) => (
+        <p key={articleId}>{paragraph}</p>
+      ))}
+    </>
   );
 };
 
