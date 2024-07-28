6c5f296225729ec844ed961a13f4b28a69056494
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-28 09:59:18 +0300

    link list to article content

--- src/pages/ArticlesList.jsx
+++ src/pages/ArticlesList.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
 import articles from "./article-content";
-const ArticlesList = ({ articleId }) => {
+const ArticlesList = () => {
   return (
     <>
       <h1>Articles</h1>
       {articles.map((article) => (
-        <div key={articleId}>
+        <Link
+          className="article-list-item"
+          to={`/articles/${article.name}`}
+          key={article.name}
+        >
           <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0, 150)} </p>
-        </div>
+          <p>{article.content[0].substring(0, 150)}... </p>
+        </Link>
       ))}
     </>
   );
