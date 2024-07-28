f1a2bb809bfc4e79ad3394f138712176977ea128
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-28 10:42:02 +0300

    create Not Found Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from "./Home.jsx";
 export { default as About } from "./About.jsx";
 export { default as Article } from "./Article.jsx";
 export { default as ArticlesList } from "./ArticlesList.jsx";
+export { default as NotFound } from "./NotFound";
