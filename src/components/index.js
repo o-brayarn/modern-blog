3e699e996014b5336f787fb4eeb07ddcf1421b71
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-30 20:51:34 +0300

    connect backend && frontend

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as NavBar } from "./NavBar.jsx";
-export {default as ArticlesListItems} from "./ArticlesListItems.jsx";
+export { default as ArticlesListItems } from "./ArticlesListItems.jsx";
+export { default as CommentsList } from "./CommentsList.jsx";
