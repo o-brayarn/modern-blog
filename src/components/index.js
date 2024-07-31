f5e5421f1b0fedf729dcd879dc1d1850d58952ff
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-31 15:36:26 +0300

    create comments form

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from "./NavBar.jsx";
 export { default as ArticlesListItems } from "./ArticlesListItems.jsx";
 export { default as CommentsList } from "./CommentsList.jsx";
+export { default as AddCommentForm } from "./AddCommentForm.jsx";
