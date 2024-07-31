b829d033adfeb51931fa8686eefbba7e4b9e3e82
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-31 15:36:50 +0300

    add upvotes and comments

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -3,7 +3,7 @@ import articles from "./article-content";
 import NotFound from "./NotFound";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { CommentsList } from "../components";
+import { AddCommentForm, CommentsList } from "../components";
 const Article = () => {
   const [articleInfo, setArticleInfo] = useState({
     upvotes: 0,
@@ -13,22 +13,39 @@ const Article = () => {
   const { articleId } = useParams();
   useEffect(() => {
     const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`);
-      const newArticleInfo = response.data;
-      setArticleInfo(newArticleInfo);
+      try {
+        const response = await axios.get(`/api/articles/${articleId}`);
+        const newArticleInfo = response.data;
+        setArticleInfo(newArticleInfo);
+      } catch (error) {
+        console.error("Failed to load article info", error);
+      }
     };
     loadArticleInfo();
-  }, [articleId]);
+  }, []);
   const article = articles.find((article) => article.name === articleId);
+
+  const addUpvote = async () => {
+    const response = await axios.put(`/api/articles/${articleId}/upvotes`);
+    const updatedArticle = response.data;
+    setArticleInfo(updatedArticle);
+  };
 
   if (!article) return <NotFound />;
   return (
     <>
       <h1>{article.title}</h1>
-      <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+      />
       <CommentsList comments={articleInfo.comments} />
     </>
   );
