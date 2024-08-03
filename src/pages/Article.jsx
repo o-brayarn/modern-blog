1c3e0edf806185aa3a6029224aaa7d222eeb3145
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-08-03 19:52:00 +0300

    make requests with auth tokens

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -18,7 +18,11 @@ const Article = () => {
   useEffect(() => {
     const loadArticleInfo = async () => {
       try {
-        const response = await axios.get(`/api/articles/${articleId}`);
+        const token = user && (await user.getIdToken());
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.get(`/api/articles/${articleId}`, {
+          headers,
+        });
         const newArticleInfo = response.data;
         setArticleInfo(newArticleInfo);
       } catch (error) {
@@ -30,7 +34,13 @@ const Article = () => {
   const article = articles.find((article) => article.name === articleId);
 
   const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+    const response = await axios.put(
+      `/api/articles/${articleId}/upvote`,
+      null,
+      { headers }
+    );
     const updatedArticle = response.data;
     setArticleInfo(updatedArticle);
   };
